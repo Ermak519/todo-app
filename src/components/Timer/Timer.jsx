06867c3f291740e772666f55c3e18db327781c30
Vyacheslav <ermak519@gmail.com>
2022-02-01 23:17:39 +0300

    fix

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -3,24 +3,22 @@ import PropTypes from 'prop-types';
 
 import './Timer.scss'
 
-export default function Timer({timerStatus}) {
+export default function Timer({ timerStatus }) {
     const [total, setTotal] = useState(0);
     const [sec, setSec] = useState(0);
     const [min, setMin] = useState(0);
-
-
 
     const tick = () => {
         if (timerStatus === 'play') {
             setTotal(total + 1);
             setSec(total % 60 ? sec + 1 : 0);
-            setMin(!(total % 60) && total !==0 ? min + 1 : min)
+            setMin(!(total % 60) && total !== 0 ? min + 1 : min)
         }
     }
 
-    useEffect(()=>{
-        const timerID = setInterval(() => {tick() }, 1000)
-        return () => {clearInterval(timerID);}
+    useEffect(() => {
+        const timerID = setInterval(() => { tick() }, 1000)
+        return () => { clearInterval(timerID); }
     })
 
     return <span className="timer">{`${min}:${sec}`}</span>
